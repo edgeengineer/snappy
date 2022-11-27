3b42fc6568cf51f8b85c31a6e08acb0561d8ab95
Timo <38291523+lovetodream@users.noreply.github.com>
2022-11-27 12:54:13 +0100

    test: add basic test case

--- Tests/SnappyTests/SnappyTests.swift
+++ Tests/SnappyTests/SnappyTests.swift
@@ -1,25 +1,34 @@
 import XCTest
 @testable import Snappy
 
-final class SnappyTests: XCTestCase {
-    func testExample() throws {
-        let data = """
+let testString = """
 1 {
-        1: "{file=Queues/QueuesCommand.swift,service=codes.vapor.application,line=122,function=startJobs(on:),source=Queues}"
-        2 {
-          1 {
-            1: 1668371605
-            2: 463043072
-          }
-          2: "[ERROR] Job run failed: RedisConnectionPoolError(baseError: RediStack.RedisConnectionPoolError.BaseError.timedOutWaitingForConnection)"
-        }
-      }
+  1: "{file=Queues/QueuesCommand.swift,service=codes.vapor.application,line=122,function=startJobs(on:),source=Queues}"
+  2 {
+    1 {
+      1: 1668371605
+      2: 463043072
+    }
+    2: "[ERROR] Job run failed: RedisConnectionPoolError(baseError: RediStack.RedisConnectionPoolError.BaseError.timedOutWaitingForConnection)"
+  }
+}
 """
-.data(using: .utf8)
-        let d = data?.compressedUsingSnappy()
-        let compressedData = try XCTUnwrap(d)
-        print(compressedData)
-        let uncompressedData = try XCTUnwrap(compressedData.decompressedUsingSnappy())
-        XCTAssertEqual(String(data: uncompressedData, encoding: .utf8), String(data: try XCTUnwrap(data), encoding: .utf8))
+
+final class SnappyTests: XCTestCase {
+    func testSimpleData() throws {
+        let data = testString.data(using: .utf8)
+        let compressedData = try XCTUnwrap(data?.compressedUsingSnappy())
+
+        if #available(macOS 13.0, *) {
+            let path = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)[0].appending(component: "compressedFile")
+
+            try compressedData.write(to: path)
+        } else {
+            // Fallback on earlier versions
+        }
+
+        let uncompressedData = try XCTUnwrap(compressedData.uncompressedUsingSnappy())
+        let uncompressedString = String(data: uncompressedData, encoding: .utf8)
+        XCTAssertEqual(uncompressedString, testString)
     }
 }
